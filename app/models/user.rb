bb25f5969ef28c7ffa89cc4e546f5b9c2b51d738
shodai <shodai-connect@tbz.t-com.ne.jp>
2016-07-18 20:31:29 +0900

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save { email.downcase! }
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_create :create_activation_digest
 
 	validates :name, presence: true, length: { maximum: 50 }
 
@@ -28,26 +29,50 @@ class User < ActiveRecord::Base
 	    update_attribute(:remember_digest, User.digest(remember_token))
 	 end
 	#渡されたトークンがダイジェストと一致したらtrueを返す
-	def authenticated?(remember_token)
-	    return false if remember_digest.nil?
-	    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+	    return false if digest.nil?
+	    BCrypt::Password.new(digest).is_password?(token)
 	 end
 	#ユーザーログインを破棄する
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+	#アカウントを有効にする
+  	def activate
+  		update_attribute(:activated,    true)
+    	update_attribute(:activated_at, Time.zone.now)
+  	end
 
-	  class << self
-    # 与えられた文字列のハッシュ値を返す
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
+  	#有効化用のメールを送信する
+  	def send_activation_email
+	    UserMailer.account_activation(self).deliver_now
+	end
 
-    # ランダムなトークンを返す
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+	class << self
+	    # 与えられた文字列のハッシュ値を返す
+	    def digest(string)
+	      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                    BCrypt::Engine.cost
+	      BCrypt::Password.create(string, cost: cost)
+	    end
+
+	    # ランダムなトークンを返す
+	    def new_token
+	      SecureRandom.urlsafe_base64
+	    end
+  	end
+
+  private
+
+  #メールアドレスを全て小文字にする
+  def downcase_email
+  	self.email = email.downcase
+  end
+
+  #有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
   end
 end
