c5fd5b123530db58a96d18cd844ac093b39fbad9
shodai <shodai-connect@tbz.t-com.ne.jp>
2016-07-18 15:44:38 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] #index,edit,update,destroyコントローラーのみ適用
+  before_action :correct_user, only: [:edit, :update] #edit,updateコントローラーのみ適用
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
   	@user = User.new
   end
@@ -18,10 +27,53 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   	def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation) #adminはアクセスの許可をしない
   	end
+
+    #beforeフィルター
+
+    #ログイン済みのユーザーかどうか確認
+    def logged_in_user
+      unless logged_in? #ログインしてないとき
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url #login画面へ移動させる
+      end
+    end
+
+    #正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+            redirect_to(root_url) unless current_user?(@user)
+            #現在のユーザーと引数のユーザーが違うときroot_urlに移動させる
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+      #現在のユーザーが管理者じゃなければroot_urlに移動させる
+    end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,63 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+	def setup
+		@user = users(:michael)
+		@other_user = users(:archer)
+	end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "shouls redirect edit when not logged in" do
+  	get :edit, id: @user
+  	assert_not flash.empty?
+  	assert_redirected_to login_url
+  end
+
+  test "shouls redirect update when not logged in" do
+  	patch :update, id: @user, user: { name: @user.name, email: @user.email }
+  	assert_not flash.empty?
+  	assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+  	log_in_as(@other_user) #other_userでlogin
+  	get :edit, id: @user #userのeditページを指定
+  	assert flash.empty? #flash出てないならtrue
+  	assert_redirected_to root_url #違うユーザーなので失敗-> root_urlへ移動される
+  end
+
+  test "should redirect update when logged in as wrong user" do
+  	log_in_as(@other_user)
+  	patch :update, id: @user, user: { name: @user.name, email: @user.email } #userのnameとemailが対象
+  	assert flash.empty? #flash出てないならtrue
+  	assert_redirected_to root_url #違うユーザーなので失敗-> root_urlへ移動される
+  end
+
+  test "should redirect destroy when not logged in" do
+    #@userを削除する命令を出しても消えてない（userの数は変わってない）
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    #@userを削除する命令を出しても消えてない（userの数は変わってない）
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
   end
 
 end
